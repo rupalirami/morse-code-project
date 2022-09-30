e507243e6577450f575b56e58a9636f010191abd
Rupali Anjaria-Rami <rupalianjaria-rami@Rupalis-iMac.home>
2022-09-29 22:02:01 -0400

    Put in toggle switch on HTML to change translator

--- index.js
+++ index.js
@@ -1,75 +1,79 @@
 export const checkValidInput = (inputString) => {
 
-    // const isValid = /^[A-Za-z0-9 ]*$/.test(inputString);
-    // console.log(isValid);
-    // if (!isValid) {
-    //     throw new Error("Cannot have special characters in your input string");
-    // } else {
-    //     return isValid;
-    // };
+    const isValid = /^[A-Za-z0-9 ]*$/.test(inputString);
+    console.log(isValid);
+    if (!isValid) {
+        return isValid;
+        throw new Error("Cannot have special characters in your input string");
+    } else {
+        return isValid;
+    };
+};
+export const clearOutput = (element) => {
+    element.value = "";
 };
 export const morseCode = (inputString) => {
     const morseCodeAlphabet = [
-    "A:.- ",
-    "B:-... ",
-    "C:-.-. ",
-    "D:-.. ",
-    "E:. ",
-    "F:..-. ",
-    "G:--. ",
-    "H:.... ",
-    "I:.. ",
-    "J:.--- ",
-    "K:-.- ",
-    "L:.-.. ",
-    "M:-- ",
-    "N:-. ",
-    "O:--- ",
-    "P:.--. ",
-    "Q:--.- ",
-    "R:.-. ",
-    "S:... ",
-    "T:- ",
-    "U:..- ",
-    "V:...-",
-    "W:.-- ",
-    "X:-..- ",
-    "Y:-.-- ",
-    "Z:--.. ",
-    "0:----- ",
-    "1:.---- ",
-    "2:..--- ",
-    "3:...-- ",
-    "4:....- ",
-    "5:..... ",
-    "6:-.... ",
-    "7:--... ",
-    "8:---.. ",
-    "9:----. ",
-    " :/ "
-];
+        "A:.- ",
+        "B:-... ",
+        "C:-.-. ",
+        "D:-.. ",
+        "E:. ",
+        "F:..-. ",
+        "G:--. ",
+        "H:.... ",
+        "I:.. ",
+        "J:.--- ",
+        "K:-.- ",
+        "L:.-.. ",
+        "M:-- ",
+        "N:-. ",
+        "O:--- ",
+        "P:.--. ",
+        "Q:--.- ",
+        "R:.-. ",
+        "S:... ",
+        "T:- ",
+        "U:..- ",
+        "V:...-",
+        "W:.-- ",
+        "X:-..- ",
+        "Y:-.-- ",
+        "Z:--.. ",
+        "0:----- ",
+        "1:.---- ",
+        "2:..--- ",
+        "3:...-- ",
+        "4:....- ",
+        "5:..... ",
+        "6:-.... ",
+        "7:--... ",
+        "8:---.. ",
+        "9:----. ",
+        " :/ "
+    ];
 
-console.log(`inputstr=${inputString}`);
-inputString = inputString.toUpperCase();
-console.log(`upper case inputstr=${inputString}`);
-//     inputString = inputString.split(" ").join("");
-// console.log(`no spaces inputstr=${inputString}`);
-const newStringArray = inputString.split("");
-console.table(`array=${newStringArray}`);
+    console.log(`inputstr=${inputString}`);
+    inputString = inputString.toUpperCase();
+    console.log(`upper case inputstr=${inputString}`);
+    //     inputString = inputString.split(" ").join("");
+    // console.log(`no spaces inputstr=${inputString}`);
+    const newStringArray = inputString.split("");
+    console.table(`array=${newStringArray}`);
 
-let morsecodeString = "";
-for (let str=0; str < newStringArray.length; str++) {
-    const stringChar = newStringArray[str];
-console.log(`index str=${str} value=${stringChar}`);
-    let morsecodeValueArray = "";
-    for (let i=0; i < morseCodeAlphabet.length; i++) {
-        if (morseCodeAlphabet[i].startsWith(stringChar)) {
-            morsecodeValueArray = morseCodeAlphabet[i].split(":");
-console.log(`morseValueArray=${morsecodeValueArray}`);
-            morsecodeString = morsecodeString + morsecodeValueArray[1];
-console.log(`morse output str=${morsecodeString}`);
+    let morsecodeString = "";
+    for (let str=0; str < newStringArray.length; str++) {
+        const stringChar = newStringArray[str];
+    console.log(`index str=${str} value=${stringChar}`);
+        let morsecodeValueArray = "";
+        for (let i=0; i < morseCodeAlphabet.length; i++) {
+            if (morseCodeAlphabet[i].startsWith(stringChar)) {
+                morsecodeValueArray = morseCodeAlphabet[i].split(":");
+    console.log(`morseValueArray=${morsecodeValueArray}`);
+                morsecodeString = morsecodeString + morsecodeValueArray[1];
+    console.log(`morse output str=${morsecodeString}`);
+            };
         };
     };
-};
-return morsecodeString.trim();
+    return morsecodeString.trim();
 }
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // import function
-import { morseCode, checkValidInput } from "./index.js";
+import { morseCode, checkValidInput, clearOutput } from "./index.js";
 
 // get DOM variables
 const textInput = document.getElementById("#input-area");
@@ -13,19 +13,23 @@ const translatedOutput = document.querySelector("#output-area");
 console.log(translatedOutput);
 
 //initialize variables
+translatedOutput.style.color = "#212529";
 
 translateButton.addEventListener("click",(event) => {
 
     const textInputValue = document.getElementById("input-area").value;
 console.log(`in btn listener input=${textInputValue}`)
     event.preventDefault(); 
-    // const isValidString = checkValidInput(textInputValue);
-    const isValidString = true;
+    const isValidString = checkValidInput(textInputValue);
+    // const isValidString = true;
 console.log(`isvalid=${isValidString}`);
     if (isValidString) {
         const morsecodeOutput = morseCode(textInputValue);
 console.log(`after func output=${morsecodeOutput}`);
         translatedOutput.innerHTML = morsecodeOutput;
+    } else {
+        translatedOutput.style.color = "#e63946";
+        translatedOutput.innerHTML = "Input string cannot have special characters"
     }
 
 });
